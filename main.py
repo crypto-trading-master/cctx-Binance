3629696cf32996271244bd288661575516e11c68
TradingMaster <trading_master@outlook.com>
2021-03-18 21:01:25 +0000

    arb_list depth

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def initialize():
 
     try:
         #Get Status of Exchange & Account
-        print("Number of Exchanges: ", len(ccxt.exchanges))
+        #print("Number of Exchanges: ", len(ccxt.exchanges))
         #print("List of available exchanges: \n\n")
         #print(ccxt.exchanges)
 
@@ -34,20 +34,47 @@ def initialize():
             'secret': config['secret']
         })
 
+        coins = ['BTC']
+
         markets = binance.load_markets()
         symbols = binance.symbols
         currencies = binance.currencies
         balances = binance.fetchBalance()
 
-        print("Exchange: ", binance.id)
-        print("Exchange limits: ", binance.rateLimit)
-        print("Number of Markets: ", len(markets))
-        print("Number of Symbols: ", len(symbols))
-        print("Number of Currencies: ", len(currencies))
+        #print("Exchange: ", binance.id)
+        #print("Exchange limits: ", binance.rateLimit)
+        #print("Number of Markets: ", len(markets))
+        #print("Number of Symbols: ", len(symbols))
+        #print("Number of Currencies: ", len(currencies))
         #pprint(balances)            
+
+        # Find Trading Pairs for base currencies
+
+        pairs = []
+
+        for symbol in symbols:
+            for coin in coins:
+                if coin in symbol:
+                    pairs.append(symbol)
+        
+        #print(pairs)
+
+        # From coin 1 to coin 2 Bid
+        # From coin 2 to coin 3 Ask        
+        # From coin 3 to coin 1 Bid
+
+        arb_list = ['ETH/BTC', 'LTC/ETH', 'LTC/BTC']
+
+        for sym in arb_list:
+            depth = binance.fetch_order_book(symbol = sym)
+            pprint(depth)
+        
 
     except():
          print("\n \n \nATTENTION: NON-VALID CCTX CONNECTION \n \n \n")        
     
+def arbitrage():
+    pass
+
 if __name__ == "__main__":
     run()
