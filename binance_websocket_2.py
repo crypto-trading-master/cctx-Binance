c3463767c635f9dfead1419defc65ff2971d6529
crypto-trading-master <trading_master@outlook.com>
2021-04-07 21:41:23 +0200

    Binance Websocket working !!

--- binance_websocket_2.py
+++ binance_websocket_2.py
@@ -1,28 +1,24 @@
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 from twisted.internet import reactor
-import json, pprint
+import json
+from pprint import pprint
+
+
+def process_message(msg):
+    # print("message type: {}".format(msg['e']))
+    pprint(msg)
 
 
 def run():
 
-    with open('secret.json', 'r') as f:
-        secretFile = json.load(f)
 
-    apiKey = secretFile['apiKey']
-    secret = secretFile['secret']
+    client = Client()
 
-    client = Client(apiKey, secret)
-    btc_price = {'error':False}
-
-    bsm = BinanceSocketManager(client)
-    conn_key = bsm.start_symbol_ticker_socket('BTCUSDT', handleTicker)
-    bsm.start()
-
-
-def handleTicker(msg):
-    pprint(msg)
-
+    bm = BinanceSocketManager(client)
+    # bm.start_trade_socket('BTCUSDT', process_message)
+    bm.start_ticker_socket(process_message)
+    bm.start()
 
 
 if __name__ == "__main__":
